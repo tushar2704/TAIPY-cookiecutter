5413bed9247d258313f9354c0040fdbf321534de
tushar27 <tushar.27041994@gmail.com>
2024-02-17 22:42:29 +0530

    update

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.2"
+
+REPO_NAME = "TAIPY-Template"
+AUTHOR_USER_NAME = "tushar2704"
+SRC_REPO = "TAIPY-Template"
+AUTHOR_EMAIL = "tushar.inseec@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="TAIPY-Template",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
